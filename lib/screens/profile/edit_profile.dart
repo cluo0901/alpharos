7aeae7e221115874f9d406fd1848e93847ed7c5d
cluo0901 <lc.luochao@gmail.com>
2021-02-21 21:12:16 +0800

    update profile image to firebase storage

--- lib/screens/profile/edit_profile.dart
+++ lib/screens/profile/edit_profile.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class EditProfile extends StatefulWidget {
   @override
@@ -6,6 +11,45 @@ class EditProfile extends StatefulWidget {
 }
 
 class _EditProfileState extends State<EditProfile> {
+  File _image;
+  final picker = ImagePicker();
+
+  Future uploadImageToFirebase(File _image) async {
+    String fileName = DateTime.now().millisecondsSinceEpoch.toString();
+    Reference firebaseStorageRef =
+        FirebaseStorage.instance.ref().child('user_profile_images/$fileName');
+    UploadTask uploadTask = firebaseStorageRef.putFile(_image);
+    TaskSnapshot taskSnapshot = await uploadTask;
+    taskSnapshot.ref.getDownloadURL().then(
+          (value) => print("Done: $value"),
+        );
+    Navigator.pop(context);
+  }
+
+  Future getImageFromGallery() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+        uploadImageToFirebase(_image);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  Future takePhoto() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+        uploadImageToFirebase(_image);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +63,46 @@ class _EditProfileState extends State<EditProfile> {
                 'Save',
                 style: TextStyle(color: Colors.white),
               ))
+        ],
+      ),
+      body: Column(
+        children: [
+          Material(
+            child: InkWell(
+              // onTap: getImage,
+              onTap: () {
+                final act = CupertinoActionSheet(
+                  title: const Text('Update profile image'),
+                  actions: <Widget>[
+                    CupertinoActionSheetAction(
+                      child: Text('Choose from gallery'),
+                      onPressed: getImageFromGallery,
+                    ),
+                    CupertinoActionSheetAction(
+                      child: Text('Take photo from camera'),
+                      onPressed: takePhoto,
+                    ),
+                  ],
+                  cancelButton: CupertinoActionSheetAction(
+                    child: Text('Cancel'),
+                    isDefaultAction: true,
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                );
+                showCupertinoModalPopup(
+                    context: context, builder: (BuildContext context) => act);
+              },
+              child: Container(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20.0),
+                  child: Image.asset('assets/images/avatar.png',
+                      width: 110.0, height: 110.0),
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
